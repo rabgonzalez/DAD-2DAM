92ee7614a6939e0c49d36e990f3acca33143dec6
rabgonzalez <rubalba.rag@gmail.com>
2024-09-25 20:23:54 +0100

    feat(JavaSportify): tarea spotify comenzada

--- JavaSpotify/dist/css.967bad95.js
+++ JavaSpotify/dist/css.967bad95.js
@@ -168,7 +168,11 @@ function reloadCSS() {
   }, 50);
 }
 module.exports = reloadCSS;
-},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"../node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"css/index.css":[function(require,module,exports) {
+var reloadCSS = require('_css_loader');
+module.hot.dispose(reloadCSS);
+module.hot.accept(reloadCSS);
+},{"./../assets/images/cover1.jpg":[["cover1.01da7a3c.jpg","assets/images/cover1.jpg"],"assets/images/cover1.jpg"],"./../assets/images/cover2.jpg":[["cover2.4ee97fb8.jpg","assets/images/cover2.jpg"],"assets/images/cover2.jpg"],"./../assets/images/cover3.jpg":[["cover3.e55ef185.jpg","assets/images/cover3.jpg"],"assets/images/cover3.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -193,7 +197,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41479" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42743" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
