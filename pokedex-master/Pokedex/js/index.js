fe2f906a93a0eef114f63870484754d7583fac79
rabgonzalez <rubalba.rag@gmail.com>
2024-09-25 19:01:29 +0100

    feat(pokedex): cambios

--- pokedex-master/Pokedex/js/index.js
+++ pokedex-master/Pokedex/js/index.js
@@ -62,7 +62,7 @@ const showPokedex = async () => {
         }
         // Para cada Pokemon, se crea una tarjeta con imágenes (vista frontal y trasera), el nombre y los tipos
         // Esta estructura HTML se añade dinámicamente al contenedor pokedex
-        pokedex.innerHTML +=    `<div class="card">
+        pokedex.innerHTML +=    `<div class="card ${tipo1} ${tipo2}">
                                     <img src="${pokemons[i].pkm_back}">
                                     <img class="front" src="${pokemons[i].pkm_front}"><br>
                                     ${pokemons[i].id}. ${pokemons[i].name}<br>
