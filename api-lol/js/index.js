bf5743be72f77a97d090a06456d4bd2a90737396
rabgonzalez <rubalba.rag@gmail.com>
2024-10-01 18:51:00 +0100

    feat(api-lol): api recogida, faltan los estilos

--- api-lol/js/index.js
+++ api-lol/js/index.js
@@ -3,25 +3,34 @@ const DOM = {
     campeones : document.getElementById('campeones'),
 }
 
-var campeones = [];
-
-boton.addEventListener('click', getCampeones);
+boton.addEventListener('click', objectToArray);
 
 async function getCampeones() {
     const url = "https://ddragon.leagueoflegends.com/cdn/13.18.1/data/es_ES/champion.json";
     try {
         const respuesta = await fetch(url);
         const datos = await respuesta.json();
-        console.log(datos.data);
-        campeones.push(campeon);
+        return datos.data;
     } catch(error) {
         console.log(error.message);
     }
-    return campeones;
 }
 
-function mostrarCampeones(datos) {
-    foreach(datos.data) {
-        
-    }
+function objectToArray() {
+    getCampeones().then((datos) => {
+        mostrarInformacion(datos);
+    });
+}
+
+function mostrarInformacion(datos){
+    const result = Object.values(datos);
+    console.log(result);
+    result.forEach((campeon) => {
+        DOM.campeones.innerHTML += `
+        <div class="card">
+            <img src="http://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/${campeon.id}.png">
+            <p>${campeon.name}</p>
+            <p>${campeon.partype}</p>
+        </div>`;
+    })
 }
